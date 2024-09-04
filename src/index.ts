2b05b7273ee3048c6317a913a724de7ce5fa64eb
LeonMendonca <leonmendonca619@gmail.com>
2024-09-04 16:53:11 +0530

    middlewares for logging, and handling error

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,40 @@
-import express, {  } from "express";
+import express from "express";
+import type { NextFunction, Request, Response } from "express";
 import { createServer } from "http";
+
+import { ClientStats, Req } from "./stats";
 
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.status(200).send('hello world');
+//chaining middleware
+app.use((req, res, next) => {
+  console.log('Request URL', req.originalUrl);
+  next(); 
+}, (req, res, next)=> {
+  console.log('Request Method', req.method);
+  next();
+});
+
+//array of middleware
+const logs = [ClientStats, Req];
+app.use(logs);
+
+app.get('/', (req: Request, res: Response) => {  
+  res.set({'Content-Type': 'application/json'}).status(200).json({message: 'server ping'});
+  console.log(res.headersSent);
+});
+
+app.use((req: Request, res: Response)=> {
+  res.status(404).json({error: `cannot ${req.method} ${req.path}`});
+});
+
+app.use((error: unknown, req: Request, res: Response, next: NextFunction)=> {
+  if(error instanceof Error) {
+    res.status(500).send(error.message);
+    return;
+  }
+  res.status(500).send('Internal server error');
 });
 
 const server = createServer(app)
