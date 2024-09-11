743ba2046d7eb4e19721d20a4495319bfdfc82fc
LeonMendonca <leonmendonca619@gmail.com>
2024-09-12 00:25:42 +0530

    moved /auth middleware below log middleware

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,22 @@ import type { Collection } from "mongodb";
 
 import { ClientStats, Req } from "./stats";
 import { MongoDbConnect } from "./database/conn";
-import type { Users, Articles } from "./database/conn";
+import { r } from "./routes";
+import { uriHelper } from "./misc/helperUri";
+import type { TUsers, TArticles } from "./database/types";
 
 
 const app = express();
 const PORT = 3000;
 
-let collectionUserInst: Collection<Users>
-let collectionArticleInst: Collection<Articles>
+//instances for database collections
+let collectionUserInst: Collection<TUsers>
+let collectionArticleInst: Collection<TArticles>
+
+//express parsers
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 
 //chaining middleware
 app.use((req, res, next) => {
@@ -27,13 +35,31 @@ app.use((req, res, next) => {
 const logs = [ClientStats, Req];
 app.use(logs);
 
+//auth router middleware
+app.use('/auth',r);
+
 app.get('/', (req: Request, res: Response) => {  
-  res.set({'Content-Type': 'application/json'}).status(200).json({message: 'server ping'});
+  res.set({'Content-Type': 'application/json'}).status(200).json({ 
+    message: 'dashboard', 
+    links: { 
+      login: '/auth/login',
+      signup: '/auth/signup',
+    } 
+  });
   console.log(res.headersSent);
 });
 
+//404 middleware
 app.use((req: Request, res: Response)=> {
-  res.status(404).json({error: `cannot ${req.method} ${req.path}`});
+  const uris = uriHelper(req.url);
+  let helperMessage = uris;
+  if(typeof uris === 'string') {
+    helperMessage = uris;
+  } else {
+    helperMessage = `Did you mean? ${uris.join(" or ")}`;
+  }
+  res.status(404).json({error: `cannot ${req.method} ${req.path}. ${helperMessage}`});
+//
 });
 
 app.use((error: unknown, req: Request, res: Response, next: NextFunction)=> {
