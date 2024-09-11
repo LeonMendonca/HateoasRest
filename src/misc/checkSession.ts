04464d45be350234ad4dab845121fbd2e375edf1
LeonMendonca <leonmendonca619@gmail.com>
2024-09-12 00:26:52 +0530

    added custom error handler

--- src/misc/checkSession.ts
+++ src/misc/checkSession.ts
@@ -1,15 +1,23 @@
 import type {Request, Response, NextFunction } from "express";
 import { getPayloadFromSid } from "./session";
+import { ErrorCode } from "../customError/error";
 
 function checkSession(req: Request, res: Response, next: NextFunction) {
-  const payload = (req.query?.sessionid) ? getPayloadFromSid(String(req.query.sessionid)) : null ;
-  if(payload === null) {
-    throw new Error('No session id provided');
-  } else if(payload === undefined) {
-    throw new Error('Not a valid session Id!');
-  } else {
-    return res.send(payload);
+  try {
+    const payload = (req.query?.sessionid) ? getPayloadFromSid(String(req.query.sessionid)) : null ;
+    if(payload === null) {
+      throw new ErrorCode('No sessionid property provided', 10002);
+    } else if(payload === undefined) {
+      throw new ErrorCode('Not a valid session Id!', 10003);
+    } else {
+      next();
+    }  
+  } catch (error) {
+    if(error instanceof ErrorCode) {
+      next(error);
+    }
   }
+  
 }
 
 export { checkSession };
