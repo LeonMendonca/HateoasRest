469334a151415fa383c32817166666df6ebd1c97
LeonMendonca <leonmendonca619@gmail.com>
2024-09-12 12:19:21 +0530

    endpoints

---
+++ src/routes.ts
@@ -0,0 +1,24 @@
+import { Router } from "express";
+import { Controllers } from "./controllers";
+import { checkSession } from "./misc/checkSession";
+import { errorHandler } from "./customError/errorHandlingMW";
+
+const r = Router();
+
+r.post('/signup', Controllers.createNewUser);
+
+r.post('/login', Controllers.loginExistingUser);
+
+r.get('/articles/all', checkSession, Controllers.getAllArticles);
+
+r.get('/articles/@me', checkSession, Controllers.getUserArticles);
+
+//r.get('/articles/@me/myarticles', checkSession, Controllers);
+
+r.post('/articles/@me/addarticle', checkSession, Controllers.createArticle)
+
+r.patch('/articles/@me/updatearticle/:articleid', checkSession, Controllers.patchArticle);
+
+r.use(errorHandler);
+
+export { r };
